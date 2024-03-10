36853a4c6982bd2bbb891d951e3233ce15c0eda6
varunwali <varunwali36@gmail.com>
2024-03-11 00:38:23 +0530

    commit

---
+++ front-end/src/components/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+
+const Footer = () => {
+  const { isAuthorized } = useContext(Context);
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By Varun.</div>
+    </footer>
+  );
+};
+
+export default Footer;
