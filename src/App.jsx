35f66b0a5ad35502027fd1b1914e319a27bda0f0
varunwali <varunwali36@gmail.com>
2024-03-11 21:24:45 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8000/api/v1/user/getuser",
+          "https://jobseek-backend.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
